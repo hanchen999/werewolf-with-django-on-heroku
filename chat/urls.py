bfdfdd99014fffbc35fda790bb5a0e1fff6753ef
han chen <hanchen@handeMacBook-Pro.local>
2017-02-20 22:41:17 -0800

    debug

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$',  views.about, name='about'),
     url(r'^create_room/$', views.create_room, name='create_room'),
-    #url(r'^join_room/$', views.join_room, name='join_room'),
     url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
 ]
