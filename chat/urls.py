bc9da5e444d3a4f7b0ea2e24fde5a3a9b21fd4d7
han chen <hanchen@handeMacBook-Pro.local>
2017-02-21 12:21:31 -0800

    update error page

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$',  views.about, name='about'),
-    url(r'^home/$',  views.home, name='homet'),
+    url(r'^home/$',  views.home, name='home'),
     url(r'^create_room/$', views.create_room, name='create_room'),
     url(r'^join_room/$', views.join_room, name='join_room'),
     url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
