86e9b6d36cce1aa5d1aa5338704e3c1c660af847
han chen <hc9999dragon@gmail.com>
2017-02-22 13:20:25 -0800

    try to send individual message

--- chat/models.py
+++ chat/models.py
@@ -19,17 +19,18 @@ class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages')
     handle = models.TextField()
     message = models.TextField()
+    typo = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
 
     def __unicode__(self):
-        return '[{timestamp}] {handle}: {message}'.format(**self.as_dict())
+        return '[{timestamp}] {handle}: {message} {typo}'.format(**self.as_dict())
 
     @property
     def formatted_timestamp(self):
         return self.timestamp.strftime('%b %-d %-I:%M %p')
     
     def as_dict(self):
-        return {'handle': self.handle, 'message': self.message, 'timestamp': self.formatted_timestamp}
+        return {'handle': self.handle, 'message': self.message, 'timestamp': self.formatted_timestamp, 'type': self.typo}
 
 class Player(models.Model):
     room = models.ForeignKey(Room, related_name='players')
