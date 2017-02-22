b5de1e15912559dd23cdd2521035a700486a295f
han chen <hanchen@handeMacBook-Pro.local>
2017-02-21 16:17:44 -0800

    check room

--- chat/consumers.py
+++ chat/consumers.py
@@ -32,8 +32,11 @@ def ws_connect(message):
     # Need to be explicit about the channel layer so that testability works
     # This may be a FIXME?
     Group('chat-'+label, channel_layer=message.channel_layer).add(message.reply_channel)
-    Group('chat-'+label, channel_layer=message.channel_layer).send({'text': 'someone Joined the room'}, immediately=True)
-
+    data = []
+    data['handle'] = 'system'
+    data['message'] = 'someone Joined the room'
+    m = room.messages.create(**data)
+    Group('chat-'+label, channel_layer=message.channel_layer).send({'text': json.dumps(m.as_dict())}, immediately=True)
     message.channel_session['room'] = room.label
 
 @channel_session
@@ -75,6 +78,10 @@ def ws_disconnect(message):
         label = message.channel_session['room']
         room = Room.objects.get(label=label)
         Group('chat-'+label, channel_layer=message.channel_layer).discard(message.reply_channel)
-        Group('chat-'+label, channel_layer=message.channel_layer).send({'text': 'someone leaves the room'}, immediately=True)
+        data = []
+        data['handle'] = 'system'
+        data['message'] = 'someone Joined the room'
+        m = room.messages.create(**data)
+        Group('chat-'+label, channel_layer=message.channel_layer).send({'text': json.dumps(m.as_dict())}, immediately=True)
     except (KeyError, Room.DoesNotExist):
         pass
