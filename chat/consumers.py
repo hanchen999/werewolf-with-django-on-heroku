3af3b66fb7ed06779584b1625de642de7f4f414b
han chen <hc9999dragon@gmail.com>
2017-02-26 10:18:38 -0800

    try to test vote

--- chat/consumers.py
+++ chat/consumers.py
@@ -369,7 +369,7 @@ def ws_receive(message):
                 sendGroupMessage(room.label, 'Game Starts!', 'message')
                 startGame(label)
         elif data['typo'] == 'Vote':
-                sendMessage(room.label, message.reply_channel.name, (voteInfo + data['message']), 'message')
+                sendMessage(room.label, message.reply_channel.name, (voteInfo + data['message']).decode('utf-8').encode('utf-8'), 'message')
         elif data['typo'] == 'posion':
             if room.gameStart == 0:
                 sendMessage(room.label, message.reply_channel.name, gameNotStarted, 'error')
