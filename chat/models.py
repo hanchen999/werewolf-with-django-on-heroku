bbeca774468234d8e52aef1d7d4d56fe6a229bc0
han chen <hc9999dragon@gmail.com>
2017-02-26 19:31:40 -0800

    try tp vote

--- chat/models.py
+++ chat/models.py
@@ -21,7 +21,6 @@ class Room(models.Model):
     duyao = models.IntegerField(default=1)
     jieyao = models.IntegerField(default=1)
     deadman = models.IntegerField(default=0)
-    status = models.IntegerField(default=0)
     def __unicode__(self):
         return self.label
 
