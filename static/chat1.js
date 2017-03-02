f1ce66786a2ea553fea98f9921a7a8bc458738f5
han chen <hc9999dragon@gmail.com>
2017-03-02 14:16:03 -0800

    fix sharen bug

--- static/chat1.js
+++ static/chat1.js
@@ -1,11 +1,19 @@
-var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
+// var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+// var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
 
 $(function() {
     // When we're using HTTPS, use WSS too.
-    //var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    //var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);    
+    var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);    
     chatsock.onmessage = function(message) {
+        var messageKeeplive = {
+            handle: 'keepalive',
+            message: 'vote',
+            typo: 'keepalive'
+        }
+        //var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+        //var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);   
+        chatsock.send(JSON.stringify(messageKeeplive));
         var data = JSON.parse(message.data);
         var chat = $("#chat")
         var ele = $('<tr></tr>')
@@ -109,24 +117,3 @@ $(function() {
         return false;
     });
 });
-
-console.log('hello world');
-
-(function my_func() {
-    // your code
-    try {
-    var message = {
-            handle: 'keepalive',
-            message: 'vote',
-            typo: 'keepalive'
-        }
-        //var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-        //var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);   
-        chatsock.send(JSON.stringify(message));
-        $("#message").val('').focus();
-        console.log(1);
-    } catch err {
-        setTimeout( my_func, 30);
-    }
-        setTimeout( my_func, 30);
-     })();
