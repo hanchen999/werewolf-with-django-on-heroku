742f8c6999c46c9ff5cde3021e86e911a3e83113
han chen <hc9999dragon@gmail.com>
2017-03-11 09:39:35 -0800

    update

--- static/chat1.js
+++ static/chat1.js
@@ -4,9 +4,9 @@
 $(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname); 
+    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + "{{room.label}}"); 
     console.log(window.location.host)   
-    console.log(window.location.pathname)
+    console.log("{{room.label}}")
     var message = {
             handle: $('#handle').val(),
             message: 'verify the socket address',
