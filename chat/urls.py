7363ecb37b6ee07bebd64c39886c2e2858ef46eb
han chen <hc9999dragon@gmail.com>
2017-03-11 09:33:43 -0800

    update

--- chat/urls.py
+++ chat/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^home/$',  views.home, name='home'),
     url(r'^create_room/$', views.create_room, name='create_room'),
     url(r'^join_room/$', views.join_room, name='join_room'),
-    url(r'^(?P<label>[\w-]{,50})(?P<position>[\w-]{,50})/$', views.chat_room, name='chat_room'),
+    url(r'^(?P<label>[\w-]{,50})/(?P<position>[\w-]{,50})/$', views.chat_room, name='chat_room'),
 ]
