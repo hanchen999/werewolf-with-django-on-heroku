9e105187b4a32907d829d1e02875c3e857ba6a80
han chen <hc9999dragon@gmail.com>
2017-03-11 14:28:25 -0800

    update

--- static/chat1.js
+++ static/chat1.js
@@ -8,13 +8,13 @@ $(function() {
     var str = temp.split("/");
     var position = str[1];
     var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat/" + position);  
-    chatsock.onopen = function(event) {
+    chatsock.onopen = function() {
         var messageKeeplive = {
             handle: $('#handle').val(),
             message: 'vote',
             typo: 'keepalive'
         }
-        console.log('send message!')
+        console.log('send message!');
         chatsock.send(JSON.stringify(messageKeeplive));
     }
     chatsock.onmessage = function(message) {
