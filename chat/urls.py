b33c51f7aa5d6b898bb46d8ee7cd704b11becc10
han chen <hc9999dragon@gmail.com>
2017-03-29 17:57:30 -0700

    add judge function

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     url(r'^create_room/$', views.create_room, name='create_room'),
     url(r'^join_room/$', views.join_room, name='join_room'),
     url(r'^judge_room/$', views.judge_room, name='judge_room'),
-    rl(r'^result/$', views.result, name='result'),
     url(r'^(?P<label>[\w-]{,50})/(?P<position>[\w-]{,50})/$', views.chat_room, name='chat_room'),
 ]
