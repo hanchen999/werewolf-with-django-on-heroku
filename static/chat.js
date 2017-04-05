55ec3f420c8e70eda89586150c760073be0d710c
han chen <hc9999dragon@gmail.com>
2017-04-05 12:00:51 -0700

    update table

--- static/chat.js
+++ static/chat.js
@@ -1,6 +1,5 @@
 // var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
 // var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
-
 $(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
